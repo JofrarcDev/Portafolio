43d8c074aded075a6cd5b0496844e07d144e18d4
Jofrandev <jofrannys@outlook.con>
2025-02-20 22:04:15 -0300

    menu responsivo agreagado

--- src/componentes/Header/header.jsx
+++ src/componentes/Header/header.jsx
@@ -1,21 +1,28 @@
 
-import React from 'react';
+
+import React, {useState} from "react";
 import jofranicon from '../../logo/jofrandev.png';
 import './header.css';  // Import the CSS file
 // Create a functional component
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="header">
         <div className="logo">
             <img src={jofranicon} className="logo" alt="logo" />
             <span>JOFRANDEV</span>
         </div>
-        <nav className="navbar">
+        <nav className={`navbar ${isOpen && "open"}`}>
             <a href="">Inicio</a>
             <a href="">Proyectos</a>
-            <a href="">Acercade mí</a>
+            <a href="">Acerca de mí</a>
             <a href="">Contacto</a>
         </nav>
+        <div className={`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)}>
+              <span></span>
+              <span></span>
+              <span></span>
+        </div>
       
     </header>
   );
