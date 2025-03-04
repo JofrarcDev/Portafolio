27ed893030caaa4e48ece831c15f3da702557b7c
Jofrandev <jofrannys@outlook.con>
2025-03-04 16:33:59 -0300

    agregado componente footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './componentes/Header/header';
 import Inicio from './componentes/Inicio/Inicio';
 import Proyectos from './componentes/proyectos/proyectos';
+import jofranicon from './logo/jofrandev.png';
 import './App.css';
 import Acerca from './componentes/Acerca/acerca';
 import Contacto from './componentes/Contacto/Contacto';
 import Habilidades from './componentes/habilidades/Habilidades';
 import Hblandas from './componentes/Hblandas/Hblandas';
+import Footer from './componentes/Footer/Footer.jsx';
 
 function App() {
   return (
@@ -18,12 +20,7 @@ function App() {
       <Habilidades />
       <Hblandas />
       <Contacto />
-      <footer>
-        <div className="container__footer">
-          <p>© 2021 JofranDev. Todos los derechos reservados.</p>
-          <a href="https://www.linkedin.com/in/jofran-ramirez-1b1a6a1b5/" target="_blank" rel="noreferrer">LinkedIn</a>
-        </div>
-      </footer>
+      <Footer />
     </div>
     
   );
---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import jofran from '../../logo/jofrandev.png';
+
+const Footer = () => {
+  return (
+    <footer>
+    <div className="container__footer">
+        <div className="footer__titulo">
+            <h2>Jofrannys Arcila</h2>
+            <span>Hacemos tu proyectos realidad</span>
+        </div>
+        <div className="footer__nav">
+            <div className="footer__nav-logo">
+                <img src={jofran} className="logo__footer" alt="logo Footer"/>
+                <span>JOFRANDEV</span>
+               
+            </div>
+            <nav>
+                <a href="#inicio">Inicio</a>
+                <a href="#proyectos">Proyectos</a>
+                <a href="#acerca">Acerca</a>
+                <a href="#habilidades">Habilidades</a>
+                <a href="#contacto">Contacto</a>
+            </nav>
+            <div className="footer__nav-social">
+                <a href="https://www.facebook.com/jofrannys.arcila" target="_blank" rel="noreferrer">
+                    <i className="fab fa-facebook"></i>
+                </a>
+                <a href="https://www.instagram.com/jofrannysarcila/" target="_blank" rel="noreferrer">
+                    <i className="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/jofrannys-arcila-6b6a6b1a2/" target="_blank" rel="noreferrer">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
